af16a3a1534faffd3c976f0787ebaded4252bfe2
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-16 17:59:08 +0700

    test function TataLetakRowColumn()

--- app/src/main/java/com/example/tataletak/MainActivity.kt
+++ app/src/main/java/com/example/tataletak/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             TataLetakTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     //panggil composable layout utama dengan padding dari Scaffold
-                    TataLetakBox(
+                    TataLetakRowColumn(
                         modifier = Modifier.padding((innerPadding))
                     )
                 }
