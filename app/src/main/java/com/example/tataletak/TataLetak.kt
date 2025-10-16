f61e086c568a743bc551f2c30d4ef627e2237183
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-16 17:19:12 +0700

    import statements androidx compose material3 n runtime

--- app/src/main/java/com/example/tataletak/TataLetak.kt
+++ app/src/main/java/com/example/tataletak/TataLetak.kt
@@ -11,3 +11,5 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
