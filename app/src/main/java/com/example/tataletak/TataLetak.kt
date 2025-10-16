a5d44775674d529688ee46372bda91ab92390a61
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-16 17:22:02 +0700

    import statements androidx compose ui

--- app/src/main/java/com/example/tataletak/TataLetak.kt
+++ app/src/main/java/com/example/tataletak/TataLetak.kt
@@ -13,3 +13,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
